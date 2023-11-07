10c1a006dd3cef15b891360d56668a60129b50e6
Andrija Petrovic <andrija.hers@gmail.com>
2023-11-07 12:10:24 +0100

    OffCanvasElement now overrides set_actual

--- dist/browserified.js
+++ dist/browserified.js
@@ -707,8 +707,26 @@ function createOffCanvasElement (lib, applib) {
 
   function OffCanvasElement (id, options) {
     DivElement.call(this, id, options);
+    this.offCanvasInstance = null;
   }
   lib.inherit(OffCanvasElement, DivElement);
+  OffCanvasElement.prototype.__cleanUp = function () {
+    this.offCanvasInstance = null;
+    DivElement.prototype.__cleanUp.call(this);
+  };
+
+  OffCanvasElement.prototype.set_actual = function (act) {
+    var ret = DivElement.prototype.set_actual.call(this, act);
+    if (ret) {
+      if (this.getConfigVal('nocontroller')) {
+        if (!this.offCanvasInstance) {
+          this.offCanvasInstance = new bootstrap.Offcanvas(this.$element[0]);
+        }
+        this.offCanvasInstance[act ? 'show' : 'hide']();
+      }
+    }
+    return ret;
+  };
 
   OffCanvasElement.prototype.hookToOffScreen = function () {
     if (!this.$element.hasClass('offcanvas')) {
